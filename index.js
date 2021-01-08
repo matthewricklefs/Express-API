f1c990368eb41a25c568e3f704af1f569710baeb
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-08 12:05:15 -0500

    fixing database connection, creating todoModel.js and creating the MongoDB Basic Schema

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
 
 const port = process.env.PORT || 4000
 const app = express()
+
+mongoose.connect("mongodb://localhost:27017/todo_db", 
+    {useNewUrlParser: true, useUnifiedTopology: true}, 
+    (err) => {
+        if(err) {
+            console.log("Mongo Connection Error: ", err)
+        }
+        else {
+            console.log("DB connected")
+        }  
+    }
+)
+
+
+app.use(cors())
+app.use(express.json())
+
+
 
 app.listen(port, () => {
     console.log( ` Server is up on port ${port}` )
