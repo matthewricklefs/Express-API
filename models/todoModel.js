97fb27f23c47d87d09860d05988021a25a3135b7
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-08 13:49:34 -0500

    virtualizes id instead of _id

--- models/todoModel.js
+++ models/todoModel.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const todos = new Schema ({
+
+    
+
     title: {
         type: String,
         maxlength: 100,
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,35 +1,35 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const Todos = require("../models/todoModel");
 
-//import Todo Model
-const Todos = require("../models/todoModel")
+// GET
+router.get("/todos", (req, res) => {
+	Todos.find((err, todos) => {
+		if (err) {
+			res.status(404).json({ message: "Get todos err", error: `${err}` });
+		} else {
+			res.status(200).json([
+				...todos.map((todo) => {
+					const { _id, title, done } = todo;
+					return { id: _id, title, done };
+				}),
+			]);
+		}
+	});
+});
 
-//GET
-router.get('/todos', (req,res) => {
-    Todos.find( (err, todos) => {
-        if(err){
-            res.status(404).json({ message: "Get todos error", error: `${err}` })
-        }
-        else {
-            res.status(200).json(todos)
-        }
-    })
-})
-
-//POST 
-router.post('/todo', (req, res) => {
-    const todo = new Todos(req.body)
-
-    todo
-        .save()
-        .then((todo) => {
-            const { _id, title, done } = todo
-
-            res.status(200).json({ id: todo._id, title, done })
-        })
-        .catch(err => {
-            res.status(400).json({ message: "Unable to post", errors: `${err}` })
-        })
-})
+// POST
+router.post("/todo", (req, res) => {
+	const todo = new Todos(req.body);
+	todo
+		.save()
+		.then((todo) => {
+			const { _id, title, done } = todo;
+			res.status(200).json({ id: _id, title, done });
+		})
+		.catch((err) => {
+			res.status(400).json({ message: "unable to post", errors: `${err}` });
+		});
+});
 
 module.exports = router;
