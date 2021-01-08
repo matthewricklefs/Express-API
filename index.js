96fd6ff07c7c379a8a0e4ae0504999a78bcdc679
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-08 11:24:09 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express")
+
+const port = process.env.PORT || 4000
+const app = express()
+
+app.listen(port, () => {
+    console.log( ` Server is up on port ${port}` )
+})
