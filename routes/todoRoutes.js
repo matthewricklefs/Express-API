12c3410f69b20519dfbea8c12e15048c06075147
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-01-08 13:36:26 -0500

    added GET and POST routes

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -6,8 +6,30 @@ const Todos = require("../models/todoModel")
 
 //GET
 router.get('/todos', (req,res) => {
-    console.log("we out here!")
-    res.end()
+    Todos.find( (err, todos) => {
+        if(err){
+            res.status(404).json({ message: "Get todos error", error: `${err}` })
+        }
+        else {
+            res.status(200).json(todos)
+        }
+    })
+})
+
+//POST 
+router.post('/todo', (req, res) => {
+    const todo = new Todos(req.body)
+
+    todo
+        .save()
+        .then((todo) => {
+            const { _id, title, done } = todo
+
+            res.status(200).json({ id: todo._id, title, done })
+        })
+        .catch(err => {
+            res.status(400).json({ message: "Unable to post", errors: `${err}` })
+        })
 })
 
 module.exports = router;
